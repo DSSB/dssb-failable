bea3ec5e207bd6e43e1a6427aceb9d71df8c1e9b
NawaMan <nawaman@dssb.io>
2018-01-09 22:09:34 -0600

    Correct the publish.sh

--- publish.sh
+++ publish.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 gradle clean build publishToMavenLocal
-cp -Rf  ~/.m2/repository/dssb-failable ../maven-repository
+
+PROJECTREPO=~/.m2/repository/dssb-failable
+PUBLISHREPO=../maven-repository
+
+if [ -d $PUBLISHREPO ]; then
+    cp -Rf $PROJECTREPO $PUBLISHREPO
+else
+    (>&2 echo "Publish repositoy does not exist!")
+fi
